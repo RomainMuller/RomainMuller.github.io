fc4999cc5ddba1c8563b7d26c3af6fd8dcea2af9
Romain Marcadier-Muller <romain.muller@telecomnancy.net>
2020-02-13 17:31:43 +0100

    chore: use gulp for asset busting (#35)

--- _plugins/custom-filters.rb
+++ _plugins/custom-filters.rb
@@ -1,23 +1,25 @@
-require 'digest'
 require 'jekyll'
+require 'json'
 
 module Jekyll
   module CustomFilters
-    def add_cache_buster(input)
-      site = @context.registers[:site]
-
-      page = site.pages.find { |p| p.url == input }
-      page ||= site.static_files.find { |f| f.url == input }
-
-      raise "Unable to locate asset for #{input}" if page.nil?
-
-      buster = Digest::SHA256.hexdigest(page.transform) if page.respond_to? :transform
-      buster ||= page.mtime if page.respond_to? :mtime
-      buster ||= site.time.to_i
-
-      relative_url "#{input}?#{buster}"
+    def fingerprint(input)
+      @manifest ||= load_manifest!
+      result = if @manifest.key? input
+        @manifest[input]
+      else
+        input
+      end
+      relative_url result
     end
   end
+end
+
+def load_manifest!
+  manifest = JSON.parse(File.read('./assets/manifest.json'))
+  manifest = manifest.transform_keys { |k| "/assets/#{k}" }
+  manifest = manifest.transform_values { |v| "/assets/#{v}" }
+  manifest
 end
 
 Liquid::Template.register_filter Jekyll::CustomFilters
---
+++ gulpfile.js
@@ -0,0 +1,55 @@
+const { spawn } = require('child_process');
+const gulp = require('gulp');
+const filter = require('gulp-filter');
+const hash = require('gulp-hash');
+const sass = require('gulp-sass');
+const sourcemaps = require('gulp-sourcemaps');
+const util = require('gulp-util');
+const path = require('path');
+
+gulp.task('css', () =>
+  gulp.src('_assets/**/*.scss')
+    .pipe(sourcemaps.init())
+    .pipe(sass({
+      outputStyle: 'compressed',
+      includePaths: path.join(__dirname, '_sass'),
+    }))
+    .pipe(hash({ algorithm: 'sha256', hashLength: 8 }))
+    .pipe(sourcemaps.write('.'))
+    .pipe(gulp.dest('assets'))
+    .pipe(hash.manifest('assets/manifest.json', {
+      append: true,
+      space: 2,
+      deleteOld: true,
+    }))
+    .pipe(gulp.dest('.'))
+);
+
+gulp.task('images', () =>
+  gulp.src('_assets/**/*')
+    .pipe(filter(['**', '!**/*.scss']))
+    .pipe(hash({ algorithm: 'sha256', hashLength: 8 }))
+    .pipe(gulp.dest('assets'))
+    .pipe(hash.manifest('assets/manifest.json', {
+      append: true,
+      space: 2,
+      deleteOld: true,
+    }))
+    .pipe(gulp.dest('.'))
+);
+
+gulp.task('assets', gulp.series(['css', 'images']));
+
+gulp.task('jekyll', () => {
+  const jekyll = spawn('bundle', ['exec', 'jekyll', 'build']);
+  const logger = buffer => buffer.toString().split(/\n/).forEach(msg => util.log(`Jekyll: ${msg}`));
+  jekyll.stdout.on('data', logger);
+  jekyll.stderr.on('data', logger);
+  return jekyll;
+});
+
+gulp.task('watch', () =>
+  gulp.watch('_assets/**/*', ['assets'])
+);
+
+gulp.task('default', gulp.series(['assets', 'jekyll']));
