058e577fcdd9093b2f5303fd5dbbad29a5f34a3f
👨🏼‍💻 Romain Marcadier-Muller <romain.muller@telecomnancy.net>
2020-04-23 17:31:48 +0200

    chore: get rid of gulp-util

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,10 @@
 const { spawn } = require('child_process');
+const log = require('fancy-log');
 const gulp = require('gulp');
 const filter = require('gulp-filter');
 const hash = require('gulp-hash');
 const sass = require('gulp-sass');
 const sourcemaps = require('gulp-sourcemaps');
-const util = require('gulp-util');
 const path = require('path');
 
 gulp.task('css', () =>
@@ -40,12 +40,19 @@ gulp.task('images', () =>
 
 gulp.task('assets', gulp.series(['css', 'images']));
 
-gulp.task('jekyll', () => {
-  const jekyll = spawn('bundle', ['exec', 'jekyll', 'build']);
-  const logger = buffer => buffer.toString().split(/\n/).forEach(msg => util.log(`Jekyll: ${msg}`));
-  jekyll.stdout.on('data', logger);
-  jekyll.stderr.on('data', logger);
-  return jekyll;
+gulp.task('jekyll', done => {
+  const process = spawn('bundle', ['exec', 'jekyll', 'build']);
+
+  const logger = buffer => buffer.toString().split(/\n/).forEach(msg => log(`Jekyll: ${msg}`));
+  process.stdout.on('data', logger);
+  process.stderr.on('data', logger);
+
+  process.on('exit', (code, signal) => {
+    if (code !== 0) {
+      return done(`Process ${code != null ? `exited with ${code}` : `killed by ${signal}`}`);
+    }
+    return done();
+  });
 });
 
 gulp.task('watch', () =>
